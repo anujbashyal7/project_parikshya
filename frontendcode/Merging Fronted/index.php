715dccefdfa0dd2f0bd88f1e6acb99eb6b74a337
pandey <pp20550403@gmail.com>
2022-08-31 13:16:23 +0545

    merging started

---
+++ frontendcode/Merging Fronted/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="/css/packages.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/main.js"></script>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="css/fontawesome-free-6.0.0-beta3-web/css/all.css">
+    <link rel="stylesheet" href="css/fontawesome-free-6.0.0-beta3-web/css/all.min.css">
+    <title>Parikshya</title>
+</head>
+
+<body>
+    <?php include 'header.html'; ?>
+    <?php include 'banner.html'; ?>
+    <?php include 'packages.html'; ?>
+    <?php include 'footer.html'; ?>
+
+</body>
