435cf51f2977ae5db30fc4fda266ec7072e8d6fc
pandey <pp20550403@gmail.com>
2022-09-15 15:48:25 +0545

    pandey updated 3:48 9/15/2022

---
+++ admin_panel/question.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/top.css">
+    <link rel="stylesheet" href="css/question.css">
+    <title>admin_panel</title>
+</head>
+
+<body>
+    <?php include 'top.html'; ?>
+    <?php include 'sidebar.html'; ?>
+    <?php include 'questions.html'; ?>
+
+</body>
+
+</html>
