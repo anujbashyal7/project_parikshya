df7b5137882555d933e34670336a19507927a405
pandey <pp20550403@gmail.com>
2022-09-18 17:43:05 +0545

    admin_panel_done

--- admin_panel/question_list.php
+++ admin_panel/question_list.php
@@ -7,12 +7,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/sidebar.css">
         <link rel="stylesheet" href="css/top.css">
-        <link rel="stylesheet" href="css/course.css">
+        <link rel="stylesheet" href="css/question_list.css">
         <title>admin_panel</title>
     </head>
     <body>
     <?php include 'top.html'; ?>
     <?php include 'sidebar.html'; ?>
-    <?php include 'course.html'; ?>
+    <?php include 'question_list.html'; ?>
+
     </body>
 </html>
+
